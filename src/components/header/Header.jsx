df679cb7b01ebb7b07ee3d4e0b7f6195dd5a57a4
YasserToni <altonyyasser@gmail.com>
2023-07-13 15:01:01 +0300

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Yasser Toni</h1>
+        <div className="name">Yasser Toni</div>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         <HeaderSocial />
