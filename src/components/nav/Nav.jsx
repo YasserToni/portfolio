6e4e6bf0f1ff3aa16b633428060c1ddb8d868620
YasserToni <altonyyasser@gmail.com>
2023-07-28 19:19:35 +0300

    first commit

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -33,42 +33,42 @@ const Nav = () => {
       <a
         href="#about"
         onClick={() => setActiveNav("#about")}
-        className={scroll > 501 && scroll < 1200 ? "active" : ""}
+        // className={scroll > 501 && scroll < 1200 ? "active" : ""}
       >
         <AiOutlineUser />
       </a>
       <a
         href="#experience"
         onClick={() => setActiveNav("#experience")}
-        className={scroll > 1201 && scroll < 1700 ? "active" : ""}
+        // className={scroll > 1201 && scroll < 1700 ? "active" : ""}
       >
         <BiBook />
       </a>
       <a
         href="#services"
         onClick={() => setActiveNav("#services")}
-        className={scroll > 1701 && scroll < 2500 ? "active" : ""}
+        // className={scroll > 1701 && scroll < 2500 ? "active" : ""}
       >
         <RiServiceLine />
       </a>
       <a
         href="#portfolio"
         onClick={() => setActiveNav("#portfolio")}
-        className={scroll > 2501 && scroll < 3400 ? "active" : ""}
+        // className={scroll > 2501 && scroll < 4700 ? "active" : ""}
       >
         <AiOutlineAppstoreAdd />
       </a>
       <a
         href="#testimonials"
         onClick={() => setActiveNav("#testimonials")}
-        className={scroll > 3401 && scroll < 4000 ? "active" : ""}
+        // className={scroll > 4701 && scroll < 5300 ? "active" : ""}
       >
         <CiStickyNote />
       </a>
       <a
         href="#contact"
         onClick={() => setActiveNav("#contact")}
-        className={scroll > 4001 && scroll < 4600 ? "active" : ""}
+        // className={scroll > 5301 && scroll < 5900 ? "active" : ""}
       >
         <BiMessageRoundedDetail />
       </a>
